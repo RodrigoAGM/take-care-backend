4e2225802627acd69c733c0d327f195ab04e42ea
RodrigoAGM <rod-gm@hotmail.com>
2020-08-14 05:51:16 -0500

    Setup backend

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+  
+app.listen(3000, function () {
+    console.log('Example app listening port 3000!');
+});
