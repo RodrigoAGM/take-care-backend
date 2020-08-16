03f423d49a600992a4d6fb9b620f7c85222852c3
RodrigoAGM <rod-gm@hotmail.com>
2020-08-16 03:10:33 -0500

    Users controller implemented

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -23,3 +23,74 @@ export async function handleAddUsers(req: Request, res: Response){
         res.status(500).send(error)
     }
 }
+
+export async function handleDeleteAllUsers(req: Request, res: Response){
+    try {
+        const data = await users.deleteAll();
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleDeleteUsers(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const data = await users.delete(id);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleGetUsersById(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const data = await users.getById(id);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleUpdateUsers(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const user:User = req.body
+        const data = await users.update(id, user);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleDeleteUsersByUsername(req: Request, res: Response){
+    try {
+        const username = req.params.username
+        const data = await users.deleteByUsername(username);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleGetUsersByUsername(req: Request, res: Response){
+    try {
+        const username = req.params.username
+        const data = await users.deleteByUsername(username);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleUpdateUsersByUsername(req: Request, res: Response){
+    try {
+        const username = req.params.username
+        const user:User = req.body
+        const data = await users.updateByUsername(username, user);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
