bd6aabb70f43c266fb97793db36dc468ca6e9a19
RodrigoAGM <rod-gm@hotmail.com>
2020-08-16 04:15:22 -0500

    Advice controller and service

---
+++ src/controller/advice.controller.ts
@@ -0,0 +1,65 @@
+import { Advices } from "../service/advice.service";
+import { Request, Response } from 'express'
+import { Advice } from "../model/advice";
+
+const advices = new Advices()
+
+export async function handleGetAdvices(req: Request, res: Response) {
+    try {
+        const data = await advices.get()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+}
+
+export async function handleAddAdvices(req: Request, res: Response){
+    try {
+        const advice:Advice = req.body
+        const data = await advices.add(advice);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleDeleteAllAdvices(req: Request, res: Response){
+    try {
+        const data = await advices.deleteAll();
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleDeleteAdvices(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const data = await advices.delete(id);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleGetAdvicesById(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const data = await advices.getById(id);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleUpdateAdvices(req: Request, res: Response){
+    try {
+        const id = req.params.id
+        const advice:Advice = req.body
+        const data = await advices.update(id, advice);
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
---
+++ src/service/advice.service.ts
@@ -0,0 +1,76 @@
+import { Advices as AdviceInterface } from "../interface/advice.interface";
+import { Advice } from "../model/advice";
+import { connect } from './db'
+import { Result, ResultId } from "../model/result";
+import { ResultSetHeader } from "mysql2";
+
+export class Advices implements AdviceInterface {
+
+    async get(): Promise<Result> {
+
+        try {
+            const conn = await connect()
+            const res = await conn.query('SELECT * FROM advices')
+            return Promise.resolve({ success: true, data: res[0] })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+
+    async add(advice: Advice): Promise<ResultId> {
+        try {
+            const conn = await connect()
+            const res = await conn.query('INSERT INTO advices SET ?', [advice])
+            const parsedRes: ResultSetHeader = res[0] as ResultSetHeader
+            return Promise.resolve({ success: true, data: advice, id: parsedRes.insertId.toString() })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+
+    async deleteAll(): Promise<Result> {
+        try {
+            const conn = await connect()
+            await conn.query('DELETE FROM advices')
+            return Promise.resolve({ success: true, data: undefined })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+
+    async delete(id: string): Promise<Result> {
+        try {
+            const conn = await connect()
+            await conn.query('DELETE FROM advices WHERE id = ?', id)
+            return Promise.resolve({ success: true, data: undefined })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+
+    async update(id: string, advice: Advice): Promise<Result> {
+        try {
+            const conn = await connect()
+            const res = await conn.query('UPDATE advices set ? WHERE id = ?', [advice, id])
+            return Promise.resolve({ success: true, data: res[0] })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+
+    async getById(id: string): Promise<Result> {
+        try {
+            const conn = await connect()
+            const res = await conn.query('SELECT * FROM advices WHERE id = ?', id)
+            return Promise.resolve({ success: true, data: res[0] })
+        } catch (error) {
+            console.error(error)
+            return Promise.reject({ success: false, error });
+        }
+    }
+}
