33047a40a8ceb323b2cb8650373290f006606713
RodrigoAGM <rod-gm@hotmail.com>
2020-08-16 04:24:55 -0500

    Add new advice controllers

--- src/controller/advice.controller.ts
+++ src/controller/advice.controller.ts
@@ -63,3 +63,34 @@ export async function handleUpdateAdvices(req: Request, res: Response){
         res.status(500).send(error)
     }
 }
+
+export async function handleDeleteUsersByType(req: Request, res: Response){
+    try {
+        const type = req.params.type
+        const data = await advices.deleteByType(type)
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleGetUsersByType(req: Request, res: Response){
+    try {
+        const type = req.params.type
+        const data = await advices.getByType(type)
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function handleUpdateUsersByType(req: Request, res: Response){
+    try {
+        const type = req.params.type
+        const advice:Advice = req.body
+        const data = await advices.updateByType(type, advice)
+        res.send(data)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
