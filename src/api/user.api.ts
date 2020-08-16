b651df232fe6b6304a1976c54590049f410fa525
RodrigoAGM <rod-gm@hotmail.com>
2020-08-16 03:15:48 -0500

    User routes implemented

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -1,9 +1,24 @@
 import {Router} from 'express'
-import { handleGetUsers, handleAddUsers } from '../controller/user.controller';
+import { handleGetUsers, 
+    handleAddUsers, 
+    handleDeleteAllUsers, 
+    handleDeleteUsers, 
+    handleGetUsersById, 
+    handleUpdateUsersByUsername, 
+    handleUpdateUsers, 
+    handleGetUsersByUsername, 
+    handleDeleteUsersByUsername } from '../controller/user.controller';
 
-const router = Router();
+const router = Router()
 
-router.get('/', handleGetUsers);
+router.get('/', handleGetUsers)
 router.post('/add', handleAddUsers)
+router.delete('/delete/all', handleDeleteAllUsers)
+router.delete('/delete/:id', handleDeleteUsers)
+router.get('/:id', handleGetUsersById)
+router.put('/update/:id', handleUpdateUsers)
+router.delete('/delete/username/:username', handleDeleteUsersByUsername)
+router.get('/username/:username', handleGetUsersByUsername)
+router.put('/update/username/:username', handleUpdateUsersByUsername)
 
 export default router
